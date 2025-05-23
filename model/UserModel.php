f16aaa47d62903ece92997c05f97f3afa6e9cd8a
mikhawa <michaeljpitz@gmail.com>
2025-05-23 08:12:14 +0200

    disconnect

--- model/UserModel.php
+++ model/UserModel.php
@@ -1,1 +1,27 @@
 <?php
+# model/UserModel.php
+
+
+/**
+ * Déconnexion de session
+ * @return boolean
+ */
+function disconnectUser(): bool
+{
+    # suppression des variables de sessions
+    session_unset();
+
+    # suppression du cookie
+    if (ini_get("session.use_cookies")) {
+        $params = session_get_cookie_params();
+        setcookie(session_name(), '', time() - 42000,
+            $params["path"], $params["domain"],
+            $params["secure"], $params["httponly"]
+        );
+    }
+
+    # Destruction du fichier lié sur le serveur
+    session_destroy();
+    return true;
+}
+?>
