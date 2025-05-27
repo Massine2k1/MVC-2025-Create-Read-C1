9e8e2df05e356e379d512e8ff339031fcdb1c4af
mikhawa <michaeljpitz@gmail.com>
2025-05-27 12:09:09 +0200

    menu

--- view/_menu.html.php
+++ view/_menu.html.php
@@ -6,7 +6,7 @@
     <ul>
         <li><a href="./">Accueil<br></a></li>
         <li><a href="./?pageChanger=about">About</a></li>
-        <li class="dropdown"><a href="#"><span>Sections</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+        <!--<li class="dropdown"><a href="#"><span>Sections</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
             <ul>
                 <li><a href="#">Dropdown 1</a></li>
                 <li class="dropdown"><a href="#"><span>Deep Dropdown</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
@@ -22,9 +22,20 @@
                 <li><a href="#">Dropdown 3</a></li>
                 <li><a href="#">Dropdown 4</a></li>
             </ul>
-        </li>
+        </li>-->
+        <?php
+        if(isset($_SESSION['user_name'])):
+        ?>
+        <li><a href="./?pageChanger=profile">Profil de <?= $_SESSION['user_name'] ?></a></li>
+            <li><a href="./?pageChanger=disconnect">Déconnexion</a></li>
+        <?php
+        else:
+        ?>
+        <li><a href="./?pageChanger=login">Connexion</a></li>
+        <?php
+        endif;
+        ?>
     </ul>
     <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
 </nav>
 
-<a class="btn-getstarted flex-md-shrink-0" href="./?pageChanger=login">Connexion</a>
